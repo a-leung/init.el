f2ca840464f9ad3c7c05d5289133012a64f063a6
Andrew Leung <1755768+a-leung@users.noreply.github.com>
2019-07-26 17:55:52 -0400

    FIX: add Melpa as package repository

--- init.el
+++ init.el
@@ -3,7 +3,10 @@
     '("marmalade" .
       "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
-    '("melpa" . "http://melpa.milkbox.net/packages/") t)
+    '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+    '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
 (package-initialize)
 
 ;; === packages to install ===
